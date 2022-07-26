4778d95fb3595485f782ae568da839ff919c8896
Coderlancce <coderlancce@gmail.com>
2022-07-26 15:39:33 -0500

    [OK] number of leaves in a node given

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count how much leaves have the node
+ *
+ * @tree: pointer to review
+ * Return: number of leaves, 0 on frailure
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->left)
+	{
+		if (tree->right)
+			leaves++;
+		if (tree->left)
+			leaves++;
+		return (leaves);
+	}
+	return (1);
+}
