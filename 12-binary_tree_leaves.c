51ea73b2ed1cb2951585a636d834cd90175a38c3
Coderlancce <coderlancce@gmail.com>
2022-07-27 09:39:17 -0500

    [OK] betty correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left || tree->left)
+	if (tree->right || tree->left)
 	{
 		if (tree->right)
 			leaves++;
