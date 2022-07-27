fcb7a66d20eca58d6591db0d40a61725e93dff92
Coderlancce <coderlancce@gmail.com>
2022-07-27 08:57:56 -0500

    [OK] betty corrections

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,15 +11,15 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if(!tree)
+	if (!tree)
 		return (0);
 
-	if(!tree->parent)
+	if (!tree->parent)
 		count++;
-	if(tree->right)
+	if (tree->right)
 		count++;
-	if(tree->left)
+	if (tree->left)
 		count++;
 
-	return (count);	  
+	return (count);
 }
